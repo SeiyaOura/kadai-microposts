460ec79f93ff494384eeafb2cf219805648cbced
seiya.oura <u776339k@ecs.osaka-u.ac.jp>
2019-05-06 19:25:56 +0000

    dependent: :destroy

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,14 +6,13 @@ class User < ApplicationRecord
                     format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i },
                     uniqueness: { case_sensitive: false }
   has_secure_password
-  has_many :microposts
+  has_many :microposts, dependent: :destroy
   has_many :relationships
   has_many :followings, through: :relationships, source: :follow
   has_many :reverses_of_relationship, class_name: 'Relationship', foreign_key: 'follow_id'
   has_many :followers, through: :reverses_of_relationship, source: :user
   has_many :favorites
   has_many :likes, through: :favorites, source: :micropost
-    
   def follow(other_user)
     unless self == other_user
       self.relationships.find_or_create_by(follow_id: other_user.id)
